e5a41c3c8498476e532b0db9d206a026b34840fe
Ghodjeezreal <fredrickokojie@yahoo.com>
2026-02-09 07:48:32 -0800

    zoom fixed

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Scoreboard App',
+        short_name: 'Scoreboard',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#222',
+        theme_color: '#001F54',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
